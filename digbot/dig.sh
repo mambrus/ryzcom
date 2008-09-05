f0bce004e950bb112b8848043b4a21573da92dec
ambrmi09 <ambrmi09>
2008-09-05 12:04:09 +0000

    First attempt to separate the event system (i.e. what is now event.sh) from dig.sh. The reason is to try to reuse the event system in other parts at either higher level or for other bots.

--- digbot/dig.sh
+++ digbot/dig.sh
@@ -28,82 +28,8 @@ fi;
 # *** Internal calibration 
 . rbot_$TOON_NAME
 
-E_MATS='\[&ITM&You found .* raw material sources.\]'
-E_DONEDIG='INF.*\[&XP&'
-E_BADDIG='INF.*\[&CHK&You need a target'
-E_NOFOCUS='INF.*\[&CHKCB&Not enough focus' 
-E_BROKENPICK='INF.*\[&CHK&You don.* forage'
-E_DMG='INF.*\[&DMG&'
-E_DEAD='INF.*\[&SYS&You have been killed'
-E_ITEMTOBAG='INF.*\[&ITM&You obtain'
-
-
-function SyncEvents {
-	echo "      SyncingEvents"
-	cp $RYZOMDIR/client.log client.log.sync	
-}
-
-#2008/08/23 13:43:56 INF   20 client_ryzom_rd.exe string_manager_client.cpp 541 : DynString 2254507851 available : [&ITM&You found 6 raw material sources.]
-
-function EventOccured {
-#	diff $RYZOMDIR/client.log client.log.sync | sed -e 's/^< //' | grep -E '617.*Rece.* CHAT' > eventscan.txt; echo $?
-	#diff $RYZOMDIR/client.log client.log.sync | sed -e 's/^< //' | grep -E $1 > $ESCANFILE
-	diff $RYZOMDIR/client.log client.log.sync | sed -e 's/^< //' > $ESCANFILE
-	#echo "Scanning for: $1"
-	grep -E "$1" < $ESCANFILE > $GREPDUMP
-	if [ $? == 0  ]; then
-		#cat $ESCANFILE
-		return 0;
-	else
-		#cat $ESCANFILE
-		return 1;
-	fi;
-}
-
-
-function SleepEvent {
-	indrag="      "
-	echo "$indrag Sleeping either for [$1]s or when [$2] happens"
-	
-	echo -n "$indrag "
-	for (( i=0 ; i<($1*10) ; i++ )) do
-		XteUSleep 50000;
-		if EventOccured "$2"; then
-			return 0;
-		fi;		
-		let "j=$i+1";
-		if [ `expr $j % 10` == 0  ]; then
-			echo -n `expr $j % 100 / 10`;
-		#else
-		#	echo -n ".";
-		fi;
-
-		if [ `expr $j % 100` == 0  ]; then
-			echo;	
-			echo -n "$indrag "
-		fi;
-	done
-	echo
-
-	return 1;
-	//SyncEvents
-}
-
-function DefaultEventHndl {
-			if EventOccured "$E_DMG"; then
-				EvadeToxicCloud
-				echo "Event found: $E_DMG";
-				exit $RC_DMG;
-			fi;
-			if EventOccured "$E_BROKENPICK"; then
-				echo "Event found: $E_BROKENPICK";
-				exit $RC_BROKENPICK;
-			fi;
-			if EventOccured "$E_DEAD"; then
-				echo "Event found: $E_DEAD";
-				exit $RC_DEAD;
-			fi;
-}
+# *** Create an event-system for this shell 
+. ./event.sh
 
 function OneCycle {
 	#SyncEvents
--- digbot/event.sh
+++ digbot/event.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. ./.env $1
+. ./.env
 
 # ** This file contains the logic to handle the event system
   
