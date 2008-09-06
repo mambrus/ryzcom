217a3ee327fb59167c039338f55b5a98bc343d6d
ambrmi09 <ambrmi09>
2008-09-06 11:07:47 +0000

    Tidy up Added lusers (list of all users)

--- digbot/rscripts.sh
+++ digbot/rscripts.sh
@@ -2,19 +2,13 @@
 . ./.env ""
 
 # Lists all users ever have said anything in any of your available chats
-function users {
-	grep -E ' String .* available.*\(.*\)' $RYZOMDIR/client.log | sed -e 's/.*\[//' | sed -e 's/\].*$//'
+function lusers {
+grep -E 'SM.*(.*)' $RYZOMDIR/client.log | sed -e 's/^.*\[//' | sed -e 's/\].*$//' | sort 
 }
 
-# Lists all users ever have said anything in any of your available chats
-function INF {
-	grep -E 'INF' $RYZOMDIR/client.log 
-}
-
-# Lists all users ever have said anything in any of your available chats
+# Tails according to pattern
 function rtail {
 	tail -f  $RYZOMDIR/client.log | grep -E "$1" | sed -e 's/.*://'
 }
 
-
-$1 $2 $3 $4
+"$@"
